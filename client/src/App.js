36d5af4b66fe4f6b10094efaf3ad28bdfe791ab6
Ari Olvera <badillo.ari@gmail.com>
2021-04-11 22:07:53 -0500

    feat: basic style & FileUpload Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import FileUpload from './components/FileUpload'
 import './App.css'
 
-const App = () => <div className='App'>app</div>
+const App = () => (
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className="fab fa-react"></i> React File Upload
+    </h4>
+
+    <FileUpload />
+  </div>
+)
 
 export default App
