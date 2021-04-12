eb88c492c3f34261301c5417087d95a633bf0f23
Ari Olvera <badillo.ari@gmail.com>
2021-04-12 10:55:01 -0500

    feat: setMessage

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,12 @@
 import { Fragment, useState } from 'react'
+import Message from './Message'
 import axios from 'axios'
 
 const FileUpload = () => {
   const [file, setFile] = useState('')
   const [filename, setFilename] = useState('Choose File')
   const [uploadedFile, setUploadedFile] = useState({})
+  const [message, setMessage] = useState('')
 
   const onChange = (e) => {
     setFile(e.target.files[0])
@@ -26,17 +28,20 @@ const FileUpload = () => {
       const { fileName, filePath } = res.data
 
       setUploadedFile({ fileName, filePath })
+
+      setMessage('File Uploaded')
     } catch (err) {
       if (err.response.status === 500) {
-        console.log('There was a problem with the server')
+        setMessage('There was a problem with the server')
       } else {
-        console.log(err.response.data.msg)
+        setMessage(err.response.data.msg)
       }
     }
   }
 
   return (
     <Fragment>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className='custom-file mb-4'>
           <input
